caba6d5242006f31142cccd263da2432215e2c0e
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-10-19 19:57:30 +0200

    selector advance

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { selectCounter, selectStep } from '../../redux/counter/selectors';
 
 
 //selectors - взяти з
 const Counter = () => {
+  const counter = useSelector(selectCounter); //take counter from the state -counter(object), counter(box)
+  const step = useSelector(selectStep); // в селекторі визначили selectStep і додали йього сюди, зміни робимо в селекторі
 
-const counter = useSelector(state => state.counter.counter) //take counter from the state -counter(object), counter(box)
-const step = useSelector(state => state.counter.step)
-
-
-
-  return (
-    <div>Counter</div>
-  )
+  return <div>Counter</div>;
 }
 
 export default Counter

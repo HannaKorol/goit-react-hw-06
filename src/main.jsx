71d7687670e956e8de1693ad302f24f105aaea16
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-10-22 20:18:31 +0200

    move and update imports for main.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../components/App.jsx";
+import App from "./components/App.jsx";
 import "modern-normalize";
 import { Provider } from "react-redux";
-import { persistor, store } from "../redux/store.js";
+import { persistor, store } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
